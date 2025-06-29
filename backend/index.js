f00e0639e3fd924049c6693226ca00e9e1190114
satriawanbasri <satriawan.basri@gmail.com>
2025-06-30 06:40:41 +0800

    setup vercel

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT
+const PORT = 4000
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization')
